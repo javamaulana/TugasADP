7b66176caaf9f2b89b5343c073a5bde15bc49c75
Java Maulana <javamaulana3000@gmail.com>
2025-05-12 21:53:59 +0700

    Add files via upload

---
+++ TugasModul5.py
@@ -0,0 +1,37 @@
+print("""
+f(x) = 4x³ + 7x - 5, x ≥ 0
+f(x) = 3x² - 5x + 1, x < 0
+g(x) = (f(x))² - f(x)
+h(x) = f(x) × g(x)
+""")
+
+x_list = []
+f_list = []
+g_list = []
+h_list = []
+ulang = 'Y'
+while ulang.upper() == 'Y':
+    n = int(input("Input banyak data n: "))
+    
+    for i in range(n):
+        x = float(input(f"Input x ke {i+1}: "))
+        x_list.append(x)
+        
+        if x >= 0:
+            f = 4 * x**3 + 7 * x - 5
+        else:
+            f = 3 * x**2 - 5 * x + 1
+        f_list.append(f)
+       
+        g = (f**2) - f
+        g_list.append(g)
+        
+        h = f * g
+        h_list.append(h)
+
+    print("\nOutput:")
+    print(f"{'No':<5}{'x':<10}{'f(x)':<15}{'g(x)':<15}{'h(x)':<15}")
+    for i in range(len(x_list)):
+        print(f"{i+1:<5}{x_list[i]:<10}{f_list[i]:<15}{g_list[i]:<15}{h_list[i]:<15}")
+   
+    ulang = input("\nInput nilai x lagi Y/N?: ")
